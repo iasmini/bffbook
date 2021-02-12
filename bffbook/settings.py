82c7e5362e706e488fdb4ca22e30f3251eb7e375
iasmini <iasmini@silexsistemas.com.br>
2021-02-12 11:07:36 -0300

    configurado para usar o postgresql

--- bffbook/settings.py
+++ bffbook/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'bffbook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bff',
+        'USER': 'bff',
+        'PASSWORD': 'bff',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
